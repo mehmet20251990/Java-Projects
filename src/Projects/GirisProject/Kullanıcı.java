5a585d1e19fec2dfefb142f4ac56b2219dadaa2a
mehmet20251990 <mehmet20251990@gmail.com>
2023-02-06 16:32:44 +0300

    update

--- src/Projects/GirisProject/Kullanıcı.java
+++ src/Projects/GirisProject/Kullanıcı.java
@@ -1,17 +1,14 @@
 package Projects.GirisProject;
 
 import java.time.LocalDateTime;
-    //task 1. stepp
 public class Kullanıcı {//kullanıcı obj uretecek kalıphane class -> main olmaz
-
+    //task 1. stepp
     String name;//obj de değer alacak inst. var.
     LocalDateTime kayıtZamanı;//obj de değer alacak inst. var.
-
     public Kullanıcı(String name, LocalDateTime kayıtZamanı) {//full p'li teleskopik cons.
         this.name = name;
         this.kayıtZamanı = kayıtZamanı;
     }
-
     @Override
     public String toString() {//bu class'dan olusturulan obj print etmek için obj datalarını string'e cevirir
         return
--- src/Projects/GirisProject/Runner.java
+++ src/Projects/GirisProject/Runner.java
@@ -2,7 +2,6 @@ package Projects.GirisProject;
 
 public class Runner {
     public static void main(String[] args) {//mo-torrrr
-
         //GirisPaneli.giris(); //ClassName ile method call
         GirisPaneli grsObj=new GirisPaneli();
         grsObj.giris();//obj ile method call
